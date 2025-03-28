f44446f9918042381b42da76e9e8e0f10464ac73
L33Z22L11 <L33Z22L11@gmail.com>
2025-03-28 19:38:15 +0800

    fix(Search): revert searchStore calling

--- app/components/ZSidebar.vue
+++ app/components/ZSidebar.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 const appConfig = useAppConfig()
 const layoutStore = useLayoutStore()
+// 触发搜索
+useSearchStore()
 
 const keycut = computed(() => navigator?.userAgent.includes('Mac OS') ? '⌘K' : 'Ctrl+K')
 </script>
@@ -13,16 +15,19 @@ const keycut = computed(() => navigator?.userAgent.includes('Mac OS') ? '⌘K' :
     <!-- 此处不能使用 Transition，因为半宽屏状态始终显示 -->
     <aside id="z-sidebar" :class="{ show: layoutStore.isOpen('sidebar') }">
         <ZhiluHeader class="sidebar-header" to="/" />
+
         <nav class="sidebar-nav scrollcheck-y">
             <div class="search-btn sidebar-nav-item gradient-card" @click="layoutStore.toggle('search')">
                 <Icon name="ph:magnifying-glass-bold" />
                 <span class="nav-text">搜索</span>
                 <span class="keycut widescreen-only">{{ keycut }}</span>
             </div>
+
             <template v-for="(group, groupIndex) in appConfig.nav" :key="groupIndex">
                 <h3 v-if="group.title">
                     {{ group.title }}
                 </h3>
+
                 <menu>
                     <li v-for="(item, itemIndex) in group.items" :key="itemIndex">
                         <ZRawLink :to="item.url" class="sidebar-nav-item" @click="layoutStore.toggle('sidebar')">
@@ -34,6 +39,7 @@ const keycut = computed(() => navigator?.userAgent.includes('Mac OS') ? '⌘K' :
                 </menu>
             </template>
         </nav>
+
         <footer class="sidebar-footer">
             <ThemeToggle />
             <ZIconNavList :list="appConfig.footer.iconNav" />
@@ -107,7 +113,8 @@ const keycut = computed(() => navigator?.userAgent.includes('Mac OS') ? '⌘K' :
     border-radius: 0.5em;
     transition: all 0.2s;
 
-    &:hover, &.router-link-active {
+    &:hover,
+    &.router-link-active {
         background-color: var(--c-bg-soft);
         color: var(--c-text);
     }
