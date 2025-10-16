efe4b04696fece284c33c10326c83a641503734f
Zhilu <hi@zhilu.cyou>
2025-10-16 23:26:44 +0800

    chore: 限制引擎版本, 优化导入, 更新友链

--- app/components/content/MusicScore.vue
+++ app/components/content/MusicScore.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { AbcVisualParams, SynthObjectController, SynthVisualOptions, TuneObject } from 'abcjs'
-import { renderAbc, synth } from 'abcjs'
 
 const props = defineProps<{
 	abc: string
@@ -36,6 +35,8 @@ async function checkSoundFonts() {
 }
 
 onMounted(async () => {
+	const { renderAbc, synth } = await import('abcjs')
+
 	tuneObj.value = renderAbc(container.value!, props.abc, abcVisualParams)[0]
 
 	if (!synth.supportsAudio() || !(await checkSoundFonts()))
